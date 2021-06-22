5e7490654dd2ddff09bdd2f3f54bccf45c99e537
siva209 <sivanarayana209@gmail.com>
2021-06-22 18:29:56 +0530

    Added:Functionality to test whether data is written to file or not

--- src/test/java/com/java/bridgelabz/employeepayroll/EmployeePayRollTest.java
+++ src/test/java/com/java/bridgelabz/employeepayroll/EmployeePayRollTest.java
@@ -1,5 +1,22 @@
 package com.java.bridgelabz.employeepayroll;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import com.bridgelabz.employee.Employee;
+import com.bridgelabz.employee.EmployeePayrollService;
+
 public class EmployeePayRollTest {
-	
+	@Test
+	public void given3Employee_WhenWrittenToFile_ShouldMatchEmployeeEntries() {
+		Employee[] arrayOfEmps = { new Employee(1, "siva", 100000), new Employee(2, "ramu", 200000),
+				new Employee(3, "sravani", 300000) };
+		EmployeePayrollService eService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+		eService.writeData(EmployeePayrollService.IOService.FILE_IO);
+		long result = eService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+		assertEquals(3, result);
+	}
 }
