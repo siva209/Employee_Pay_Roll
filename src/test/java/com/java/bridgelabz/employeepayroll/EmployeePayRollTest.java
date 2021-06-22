62d2903e3b6230667c62ccf46c72142f14a95cdd
siva209 <sivanarayana209@gmail.com>
2021-06-22 19:04:24 +0530

    Added::Test case to check whether correct employee entry is printed

--- src/test/java/com/java/bridgelabz/employeepayroll/EmployeePayRollTest.java
+++ src/test/java/com/java/bridgelabz/employeepayroll/EmployeePayRollTest.java
@@ -29,4 +29,13 @@ public class EmployeePayRollTest {
 		long result = eService.countEntries(EmployeePayrollService.IOService.FILE_IO);
 		assertEquals(3, result);
 	}
+	public void given3Employee_WhenWrittenToFile_ShouldReturnEmployeeEntries() {
+		Employee[] arrayOfEmps = { new Employee(1, "Jeff Bezos", 100000), new Employee(2, "Bill Gates", 200000),
+				new Employee(3, "Mark Zuckerberg", 300000) };
+		EmployeePayrollService eService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+		eService.writeData(EmployeePayrollService.IOService.FILE_IO);
+		eService.printData(EmployeePayrollService.IOService.FILE_IO);
+		long result = eService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+		assertEquals(3, result);
+	}
 }
