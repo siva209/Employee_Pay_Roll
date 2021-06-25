c6e85ebed33936728375842ef1d0d9739e9280e0
siva209 <sivanarayana209@gmail.com>
2021-06-25 11:27:07 +0530

    Added:Test case to check for reading of employee from a file

--- src/test/java/com/java/bridgelabz/employeepayroll/EmployeePayRollTest.java
+++ src/test/java/com/java/bridgelabz/employeepayroll/EmployeePayRollTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.bridgelabz.employee.Employee;
@@ -29,14 +30,26 @@ public class EmployeePayRollTest {
 		long result = eService.countEntries(EmployeePayrollService.IOService.FILE_IO);
 		assertEquals(3, result);
 	}
+	
+	//count entries
 	@Test
-	public void given3Employee_WhenWrittenToFile_ShouldReturnEmployeeEntries() {
-		Employee[] arrayOfEmps = { new Employee(1, "siva", 100000), new Employee(2, "ramu", 200000),
-				new Employee(3, "sravani", 300000) };
-		EmployeePayrollService eService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
-		eService.writeData(EmployeePayrollService.IOService.FILE_IO);
-		eService.printData(EmployeePayrollService.IOService.FILE_IO);
-		long result = eService.countEntries(EmployeePayrollService.IOService.FILE_IO);
-		assertEquals(3, result);
+		public void given3Employee_WhenWrittenToFile_ShouldReturnEmployeeEntries() {
+			Employee[] arrayOfEmps = { new Employee(1, "siva", 100000), new Employee(2, "ramu", 200000),
+					new Employee(3, "sravani", 300000) };
+			EmployeePayrollService eService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+			eService.writeData(EmployeePayrollService.IOService.FILE_IO);
+			eService.printData(EmployeePayrollService.IOService.FILE_IO);
+			long result = eService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+			assertEquals(3, result);
+		}
+	
+	 @Test
+	    public void givenFile_onReadingFromFile_shouldMatchEmployeeCount() {
+	        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+	        employeePayrollService.readDataFromFile(EmployeePayrollService.IOService.FILE_IO);
+	        long entries = employeePayrollService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+	        Assert.assertEquals(3, entries);
+	    }
 	}
-}
+
+	
