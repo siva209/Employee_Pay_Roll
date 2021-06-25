4213d5e90ae46cfb1287d347082b6e44b66e88cb
siva209 <sivanarayana209@gmail.com>
2021-06-25 12:38:06 +0530

    Added:Test cases to check whether the records are retrieved or not

--- src/test/java/com/java/bridgelabz/employeepayroll/EmployeePayRollTest.java
+++ src/test/java/com/java/bridgelabz/employeepayroll/EmployeePayRollTest.java
@@ -3,6 +3,7 @@ package com.java.bridgelabz.employeepayroll;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
+import java.util.*;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,45 +12,54 @@ import com.bridgelabz.employee.Employee;
 import com.bridgelabz.employee.EmployeePayrollService;
 
 public class EmployeePayRollTest {
-	@Test
-	public void given3Employee_WhenWrittenToFile_ShouldMatchEmployeeEntries() {
-		Employee[] arrayOfEmps = { new Employee(1, "siva", 100000), new Employee(2, "ramu", 200000),
-				new Employee(3, "sravani", 300000) };
-		EmployeePayrollService eService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
-		eService.writeData(EmployeePayrollService.IOService.FILE_IO);
-		long result = eService.countEntries(EmployeePayrollService.IOService.FILE_IO);
-		assertEquals(3, result);
-	}
-	@Test
-	public void given3Employee_WhenWrittenToFile_ShouldPrintEmployeeEntries() {
-		Employee[] arrayOfEmps = { new Employee(1, "siva ", 100000), new Employee(2, "ramu", 200000),
-				new Employee(3, "sravani", 300000) };
-		EmployeePayrollService eService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
-		eService.writeData(EmployeePayrollService.IOService.FILE_IO);
-		eService.printData(EmployeePayrollService.IOService.FILE_IO);
-		long result = eService.countEntries(EmployeePayrollService.IOService.FILE_IO);
-		assertEquals(3, result);
-	}
-	
-	//count entries
-	@Test
-		public void given3Employee_WhenWrittenToFile_ShouldReturnEmployeeEntries() {
-			Employee[] arrayOfEmps = { new Employee(1, "siva", 100000), new Employee(2, "ramu", 200000),
-					new Employee(3, "sravani", 300000) };
-			EmployeePayrollService eService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
-			eService.writeData(EmployeePayrollService.IOService.FILE_IO);
-			eService.printData(EmployeePayrollService.IOService.FILE_IO);
-			long result = eService.countEntries(EmployeePayrollService.IOService.FILE_IO);
-			assertEquals(3, result);
-		}
-	
+//	@Test
+//	public void given3Employee_WhenWrittenToFile_ShouldMatchEmployeeEntries() {
+//		Employee[] arrayOfEmps = { new Employee(1, "siva", 100000), new Employee(2, "ramu", 200000),
+//				new Employee(3, "sravani", 300000) };
+//		EmployeePayrollService eService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+//		eService.writeData(EmployeePayrollService.IOService.FILE_IO);
+//		long result = eService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+//		assertEquals(3, result);
+//	}
+//	@Test
+//	public void given3Employee_WhenWrittenToFile_ShouldPrintEmployeeEntries() {
+//		Employee[] arrayOfEmps = { new Employee(1, "siva ", 100000), new Employee(2, "ramu", 200000),
+//				new Employee(3, "sravani", 300000) };
+//		EmployeePayrollService eService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+//		eService.writeData(EmployeePayrollService.IOService.FILE_IO);
+//		eService.printData(EmployeePayrollService.IOService.FILE_IO);
+//		long result = eService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+//		assertEquals(3, result);
+//	}
+//	
+//	//count entries
+//	@Test
+//		public void given3Employee_WhenWrittenToFile_ShouldReturnEmployeeEntries() {
+//			Employee[] arrayOfEmps = { new Employee(1, "siva", 100000), new Employee(2, "ramu", 200000),
+//					new Employee(3, "sravani", 300000) };
+//			EmployeePayrollService eService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+//			eService.writeData(EmployeePayrollService.IOService.FILE_IO);
+//			eService.printData(EmployeePayrollService.IOService.FILE_IO);
+//			long result = eService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+//			assertEquals(3, result);
+//		}
+//	
+//	 @Test
+//	    public void givenFile_onReadingFromFile_shouldMatchEmployeeCount() {
+//	        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+//	        employeePayrollService.readDataFromFile(EmployeePayrollService.IOService.FILE_IO);
+//	        long entries = employeePayrollService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+//	        Assert.assertEquals(3, entries);
+//	    }
+//	 
 	 @Test
-	    public void givenFile_onReadingFromFile_shouldMatchEmployeeCount() {
-	        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
-	        employeePayrollService.readDataFromFile(EmployeePayrollService.IOService.FILE_IO);
-	        long entries = employeePayrollService.countEntries(EmployeePayrollService.IOService.FILE_IO);
-	        Assert.assertEquals(3, entries);
-	    }
+		public void given3Employee_WhenRetrieved_ShouldMatchEmployeeCount() {
+			List<Employee> employees = new ArrayList<>();
+			EmployeePayrollService eService = new EmployeePayrollService(employees);
+			employees = eService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO);
+			System.out.println(employees);
+			assertEquals(3, employees.size());
+		}
 	}
 
 	
